4794ce2152af7bf32bf7a40ee1b76e5aa4852f63
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-08-24 23:54:10 -0300

    Add pool

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,25 @@
-module.exports = function(app){
+//module.exports = function(app){
+exports.pool = function(app){
+    const mysql = require('../views/config/config.json').pool
     app.get('/noticias', function(req,res){
 
-        var mysql = require('mysql');
+        const mysql = require('mysql')
 
-        var connection = mysql.createConnection({
+        var pool = mysql.createPool({
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+            port: process.env.MYSQL_PORT
+        })
+
+        /*var connection = mysql.createConnection({
             host: 'localhost',
             user: 'root',
             password: 'root',
             database: 'portal_noticias',
             insecureAuth: true
-        });
+        });*/
 
         connection.connect(function(err) {
             if (err){
