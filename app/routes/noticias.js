34bf818bbcb510b93dca4ce92ffc9dd8b79f597c
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-08-27 20:34:22 -0300

    Updated noticias.js

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -11,9 +11,7 @@ module.exports = function(app){
         });
 
         connection.query("SELECT * FROM noticias", function(error, result){
-            res.send(result);
+            res.render('noticias/noticias', { noticias : result });
         });
-
-        //res.render('noticias/noticias');
     });
 }
