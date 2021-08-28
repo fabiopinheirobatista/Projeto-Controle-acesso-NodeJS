9dae2258bac1e362faca589c6728e8551093f372
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-08-27 22:04:56 -0300

    Updated connection between DB and noticies

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,16 +1,11 @@
-module.exports = function(app){
+var dbConnection = require('../../config/dbConnection')
+
+module.exports = (app) => {
+    var connection = dbConnection()
+    
     app.get('/noticias', function(req,res){
 
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'portal_noticias'
-        });
-
-        connection.query("SELECT * FROM noticias", function(error, result){
+        connection.query("SELECT * FROM noticias", (error, result) => {
             res.render('noticias/noticias', { noticias : result });
         });
     });
