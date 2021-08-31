c6390016fd49936c118a69ce3e2d4e3754fd0305
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-08-30 23:34:01 -0300

    Added noticia.js_noticia.model
    Added notcia.js (route) / noticia.ejs (model)

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,11 +1,8 @@
 //var dbConnection = require('../../config/dbConnection')
 
 module.exports = (app) => {
-    
     app.get('/noticias', function(req,res){
-
         var connection = app.config.dbConnection()
-        
         connection.query("SELECT * FROM noticias", (error, result) => {
             res.render('noticias/noticias', { noticias : result });
         });
