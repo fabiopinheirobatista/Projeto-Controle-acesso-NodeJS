8bc4cae187f205cf2c0827f79fc0e2ef65e00d44
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-09-03 18:14:28 -0300

    Added BODY-PARSER library

---
+++ app/routes/admin.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+	app.get('/formulario_inclusao_noticia', (req, res) => {
+		res.render("admin/form_add_noticia")
+	})
+
+	app.post('/noticias/salvar', (req, res) => {
+		var noticias = req.body
+		res.send(noticias)
+	})
+}
