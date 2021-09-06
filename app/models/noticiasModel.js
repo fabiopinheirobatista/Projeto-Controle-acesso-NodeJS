0a81bcb0156063719adfa758e051474f80e8e2a8
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-09-06 00:04:28 -0300

    Testing

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -1,17 +1,17 @@
-module.exports = function(){
+module.exports = () => {
 
-    this.getNoticias = function(connection, callback){
-        connection.query('SELECT * FROM noticias', callback);
+    this.getNoticias = (connection, callback) => {
+        connection.query('SELECT * FROM noticias', callback)
     }
 
-    this.getNoticia = function(connection, callback){
-        connection.query('SELECT * FROM noticias WHERE id_noticia = 2', callback);
+    this.getNoticia = (connection, callback) => {
+        connection.query('SELECT * FROM noticias WHERE id_noticia = 2', callback)
     }
 
-    this.salvarNoticia = function(noticia, connection, callback){
-        connection.query('INSERT INTO noticias SET ?', noticia, callback);
+	this.salvarNoticia = (noticia, connection, callback) => {
+        connection.query('INSERT INTO noticias SET ?', noticia, callback)
     }
 
-    return this;
+    return this
 
 }
