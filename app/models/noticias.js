a9b007dd58946cc5d8baf4491f29917445464412
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-09-07 20:04:06 -0300

    Added validation with express-validator

--- app/models/noticias.js
+++ app/models/noticias.js
@@ -1,13 +1,12 @@
-module.exports = function(application){
+module.exports = (application) => {
 
-    application.get('/noticias', function(req,res){
+    application.get('/noticias', (req,res) => {
 
-        var connection = application.config.dbConnection();
-        var noticiasModel = application.app.models.noticiasModel;
+        var connection = application.config.dbConnection()
+        var noticiasModel = application.app.models.noticiasModel
 
-        noticiasModel.getNoticias(connection, function(error, result){
-            res.render('noticias/noticias', { noticias : result });
-        });
-
-    });
+        noticiasModel.getNoticias(connection, (error, result) => {
+            res.render('noticias/noticias', { noticias : result })
+        })
+    })
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,31 @@ module.exports = (application) => {
 
     application.post('/noticias/salvar', (req,res) => {
         var noticia = req.body
+
+        req.assert('titulo','Título é obrigatório').notEmpty()
+        req.assert('resumo','Resumo é obrigatório').notEmpty()
+        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100)
+        req.assert('autor','Autor é obrigatório').notEmpty()
+        
+        req.assert('data_noticia','Data é obrigatória').notEmpty().isDate({format: 'YYYY-MM-DD'})
+
+        var erros = req.validationErrors()
+        if(erros){
+            res.render('admin/form_add_noticia')
+            return
+        }
+
+        /*function isValidDate(value) {
+            if (!value.match(/^\d{4}-\d{2}-\d{2}$/))
+                return false
+            
+            const date = new Date(value)
+            if (!date.getTime())
+                return false
+            return date.toISOString().slice(0, 10) === value
+        }
+            
+        req.assert('data_noticia').custom(isValidDate).withMessage('A data é inválida')*/
         
 		var connection = application.config.dbConnection()
 		var noticiasModel = application.app.models.noticiasModel
