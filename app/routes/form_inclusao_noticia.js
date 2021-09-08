bfa247b2b55b2442822ea3e1cb6589d6e92f34b0
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-09-08 16:15:32 -0300

    Add validation on fields in the FORM ADD NOTICIES

--- app/routes/form_inclusao_noticia.js
+++ app/routes/form_inclusao_noticia.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-    app.get('/formulario_inclusao_noticia', (req,res) => {
-        res.render('admin/form_add_noticia')
+    app.get('/formulario_inclusao_noticia', (req, res) => {
+        res.render('admin/form_add_noticia', {validacao : {}})
     })
 }
