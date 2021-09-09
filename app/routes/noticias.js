6ef871e870c6db0c3c8a54be68c1079e8d173e81
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2021-09-08 23:10:04 -0300

    Added security

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -8,4 +8,14 @@ module.exports = (application) => {
 			res.render('noticias/noticias', {noticias : result})
 		})
 	})
+
+	application.get('/noticia', (req, res) => {
+	
+		var connection = application.config.dbConnection()
+		var noticiasModel = application.app.models.noticiasModel
+
+		noticiasModel.getNoticia(connection, (error, result) => {
+			res.render('noticias/noticia', {noticia : result})
+		})
+	});
 }
